5001a2bacc14123efead5523afbe8fe6fdc73209
amin mohtashami <amin123096@icloud.com>
2024-07-15 19:14:48 -0700

    Initial Commit

---
+++ iluminoTakeHomeAsses/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  iluminoTakeHomeAsses
+//
+//  Created by Amin Mohtashami on 7/15/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
